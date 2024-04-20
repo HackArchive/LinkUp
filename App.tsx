e48288bc0f558816bbd8c433f8958a02f73027a5
Ankit Kokane <ankitkokane90@gmail.com>
2024-04-20 16:23:40 +0530

    change in directory structure

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Hello Hello</Text>
+      <Text>Hello Hello Hello Hello</Text>
       <StatusBar style="auto" />
     </View>
   );
