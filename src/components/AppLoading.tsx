1cd84a902982996d0f399102aae65d31f4132982
Ankit Kokane <ankitkokane90@gmail.com>
2024-04-21 12:48:50 +0530

    App Loading state

---
+++ src/components/AppLoading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function AppLoading() {
+    return (
+        <View>
+            <Text>
+                AppLoading
+                </Text>
+        </View>
+    )
+}
