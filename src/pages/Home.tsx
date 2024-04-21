47908278893e3e81db36e3e70bcb94803a0d6d5f
Ankit Kokane <ankitkokane90@gmail.com>
2024-04-21 17:09:53 +0530

    Updated Home UI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -21,24 +21,35 @@ import {
 
 export default function Home() {
   return (
-    <View>
-      <View className="h-14"></View>
+    <View className="bg-[#1c1c1c] text-white">
 
-      <Text className="font-bold text-lg text-center my-4">
-        Link<Text className="text-pink-500">Up</Text>
-      </Text>
+      <View className="flex flex-row items-center justify-between w-full px-4">
+        <Text className="font-[Billa-Bong] text-3xl my-4 text-white">
+          Pixel<Text className="text-pink-500">Pulse</Text>
+        </Text>
+        <Pressable className="w-12 py-2 ml-auto">
+          <Image
+            source={{
+              uri: "https://pbs.twimg.com/profile_images/1699481797158121472/Q0Cefxy8_400x400.jpg",
+            }}
+            style={{ width: 40, height: 40, borderRadius: 5 }}
+            width={40}
+            height={40}
+          />
+        </Pressable>
 
+      </View>
       <ScrollView>
         <View className="flex flex-col gap-y-2 py-2">
           {Array.from({ length: 13 }).map((_, index) => (
-            <>
+            <View key={index}>
               <View className="flex flex-row gap-2 w-full px-4">
                 <View className="w-16">
                   <Image
                     source={{
                       uri: "https://pbs.twimg.com/profile_images/1699481797158121472/Q0Cefxy8_400x400.jpg",
                     }}
-                    style={{ width: 50, height: 50, borderRadius: 50 }}
+                    style={{ width: 50, height: 50, borderRadius: 10 }}
                     width={50}
                     height={50}
                   />
@@ -46,13 +57,13 @@ export default function Home() {
 
                 <View className="flex-1 flex flex-col">
                   <View className="flex flex-row gap-x-2 items-center">
-                    <Text className="font-bold text-md">Vape Lover</Text>
+                    <Text className="font-bold text-lg text-white">Vape Lover</Text>
                     <Text className="text-gray-500 text-sm">• 1d ago</Text>
                   </View>
 
                   <View className="h-2"></View>
 
-                  <Text className="break-words">
+                  <Text className="break-words text-white text-base">
                     I'm a vape lover, I absolutely love vapes, especially the
                     blueberry one. I really hope they leagalize it and allow me
                     to carry one in airports and also in college lecture
@@ -64,22 +75,22 @@ export default function Home() {
                     source={{
                       uri: "https://pbs.twimg.com/media/GLrHV1fbcAAA870?format=jpg&name=large",
                     }}
-                    height={300}
-                    className="rounded-xl"
+                    height={250}
+                    className="rounded-xl object-cover"
                   />
 
                   <View className="h-3"></View>
 
                   <View className="flex flex-row gap-2 justify-between">
-                    <Heart color="gray" size={16} />
-                    <MessageCircle color="gray" size={16} />
-                    <Share2 color="gray" size={16} />
+                    <Heart color="gray" size={20} />
+                    <MessageCircle color="gray" size={20} />
+                    <Share2 color="gray" size={20} />
                   </View>
                 </View>
               </View>
 
-              <View className="h-[0px] my-4 border-b border-gray-300"></View>
-            </>
+              <View className="h-[0px] my-4 border-b border-[#2b2b2b]"></View>
+            </View>
           ))}
         </View>
       </ScrollView>
@@ -87,89 +98,89 @@ export default function Home() {
   );
 }
 
-export function PostCard() {
-  return (
-    <Pressable
-      className="bg-[#131313] p-4 my-2 rounded-2xl w-full"
-      onPress={() => {}}
-    >
-      <View className="flex flex-row justify-center gap-x-2 items-center">
-        <View className="flex flex-row">
-          {/* {Array.from({ length: 3 }).map((avatar, i) => (
-            <Avatar.Image
-              source={{
-                uri: avatar,
-              }}
-              size={24}
-              className={i > 0 ? "-ml-2" : ""}
-            />
-          ))} */}
-        </View>
+// export function PostCard() {
+//   return (
+//     <Pressable
+//       className="bg-[#131313] p-4 my-2 rounded-2xl w-full"
+//       onPress={() => { }}
+//     >
+//       <View className="flex flex-row justify-center gap-x-2 items-center">
+//         <View className="flex flex-row">
+//           {/* {Array.from({ length: 3 }).map((avatar, i) => (
+//             <Avatar.Image
+//               source={{
+//                 uri: avatar,
+//               }}
+//               size={24}
+//               className={i > 0 ? "-ml-2" : ""}
+//             />
+//           ))} */}
+//         </View>
 
-        <Text className="text-white text-center">{"peerMessage"}</Text>
-      </View>
+//         <Text className="text-white text-center">{"peerMessage"}</Text>
+//       </View>
 
-      {/* <View className="w-[90%] mx-auto h-[1px] my-3 bg-white/50" /> */}
+//       {/* <View className="w-[90%] mx-auto h-[1px] my-3 bg-white/50" /> */}
 
-      <View
-        className={twMerge(
-          "w-[90%] mx-auto h-[1px] my-3 bg-white/0 border-t border-white/50 border-r-1",
-          Platform.OS === "android" ? "border-dashed" : ""
-        )}
-      />
+//       <View
+//         className={twMerge(
+//           "w-[90%] mx-auto h-[1px] my-3 bg-white/0 border-t border-white/50 border-r-1",
+//           Platform.OS === "android" ? "border-dashed" : ""
+//         )}
+//       />
 
-      <View className="flex flex-row" style={{ marginBottom: 10 }}>
-        <Image
-          source={{ uri: "event.image" }}
-          style={{ width: 69, borderRadius: 15, marginRight: 20 }}
-        />
+//       <View className="flex flex-row" style={{ marginBottom: 10 }}>
+//         <Image
+//           source={{ uri: "event.image" }}
+//           style={{ width: 69, borderRadius: 15, marginRight: 20 }}
+//         />
 
-        <View className="flex gap-1">
-          <Text className="text-white text-lg">ETH Async Hack</Text>
-          <View className="flex flex-row gap-1 items-center">
-            <LocateIcon size={20} color="#989898" />
-            <Text className="text-gray-200 font-sans">Online</Text>
-          </View>
-          <View className="flex flex-row gap-1 items-center">
-            <Calendar size={20} color="#989898" />
-            <Text className="text-gray-200">{"time.toDateString()"}</Text>
-          </View>
-        </View>
-      </View>
+//         <View className="flex gap-1">
+//           <Text className="text-white text-lg">ETH Async Hack</Text>
+//           <View className="flex flex-row gap-1 items-center">
+//             <LocateIcon size={20} color="#989898" />
+//             <Text className="text-gray-200 font-sans">Online</Text>
+//           </View>
+//           <View className="flex flex-row gap-1 items-center">
+//             <Calendar size={20} color="#989898" />
+//             <Text className="text-gray-200">{"time.toDateString()"}</Text>
+//           </View>
+//         </View>
+//       </View>
 
-      <View className="flex flex-row justify-between">
-        <View className="flex flex-row gap-4">
-          <View className="flex flex-row gap-1 items-center">
-            <UsersIcon size={24} color="#989898" />
+//       <View className="flex flex-row justify-between">
+//         <View className="flex flex-row gap-4">
+//           <View className="flex flex-row gap-1 items-center">
+//             <UsersIcon size={24} color="#989898" />
 
-            <Text className="text-white font-light">0</Text>
-          </View>
+//             <Text className="text-white font-light">0</Text>
+//           </View>
 
-          <View className="flex flex-row gap-1 items-center">
-            <DollarSign size={24} color="#989898" />
-            <Text className="text-white font-light">Free</Text>
-          </View>
-        </View>
+//           <View className="flex flex-row gap-1 items-center">
+//             <DollarSign size={24} color="#989898" />
+//             <Text className="text-white font-light">Free</Text>
+//           </View>
+//         </View>
 
-        {true ? (
-          <TouchableOpacity
-            className="rounded-full p-3 px-3 bg-[#54ADFF]"
-            onPress={() => {}}
-          >
-            <MessageCircle size={24} color="white" />
-          </TouchableOpacity>
-        ) : (
-          <TouchableOpacity
-            className="rounded-full p-2 px-3 bg-[#1E1E1E]"
-            disabled={false}
-            onPress={() => {}}
-          >
-            <Text className="font-bold" style={{ color: "#A5219B" }}>
-              Join them
-            </Text>
-          </TouchableOpacity>
-        )}
-      </View>
-    </Pressable>
-  );
-}
+//         {true ? (
+//           <TouchableOpacity
+//             className="rounded-full p-3 px-3 bg-[#54ADFF]"
+//             onPress={() => { }}
+//           >
+//             <MessageCircle size={24} color="white" />
+//           </TouchableOpacity>
+//         ) : (
+//           <TouchableOpacity
+//             className="rounded-full p-2 px-3 bg-[#1E1E1E]"
+//             disabled={false}
+//             onPress={() => { }}
+//           >
+//             <Text className="font-bold" style={{ color: "#A5219B" }}>
+//               Join them
+//             </Text>
+//           </TouchableOpacity>
+//         )}
+//       </View>
+//     </Pressable>
+//   );
+// }
